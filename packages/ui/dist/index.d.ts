12df21dc0b038f6439b45ff0327791420c21f2df
Miguel Gargallo <mgargallo16@gmail.com>
2022-12-05 13:48:20 +0100

    Source Code of Next13 Image Gallery Turbo

---
+++ packages/ui/dist/index.d.ts
@@ -0,0 +1,21 @@
+declare const Button: () => JSX.Element;
+
+declare const Menu: () => JSX.Element;
+
+declare const MenuLogo: () => JSX.Element;
+
+declare const TitlePylar: () => JSX.Element;
+
+declare const ListPylar: () => JSX.Element;
+
+declare const Version: () => JSX.Element;
+
+declare const Footer: () => JSX.Element;
+
+declare const Scrollable: () => JSX.Element;
+
+declare const blog: () => JSX.Element;
+
+declare const post12032022: () => JSX.Element;
+
+export { Button, Footer, ListPylar, Menu, MenuLogo, Scrollable, TitlePylar, Version, blog, post12032022 };
